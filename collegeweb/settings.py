d189043980a071ac2f37d7f0f4d2ba9dc500ba36
Akshay-appu <appu03626@gmail.com>
2025-11-05 21:10:55 +0530

    Update settings.py

--- collegeweb/settings.py
+++ collegeweb/settings.py
@@ -140,6 +140,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
