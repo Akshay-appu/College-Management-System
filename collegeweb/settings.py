11937b1687b2975e093c9078bee011d8940162a4
Akshay-appu <appu03626@gmail.com>
2025-11-05 21:15:25 +0530

    Remove STATIC_ROOT configuration
    Remove STATIC_ROOT setting from settings.py

--- collegeweb/settings.py
+++ collegeweb/settings.py
@@ -140,7 +140,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
